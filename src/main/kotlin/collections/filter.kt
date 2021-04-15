fe331aba521cdf47b26ca830a08338f1cb0908d0
abdul <abdul.tanveer@gmail.com>
2021-04-15 16:33:51 +0530

    count

--- src/main/kotlin/collections/filter.kt
+++ src/main/kotlin/collections/filter.kt
@@ -5,6 +5,9 @@ class filter {
 val nos = listOf<Int>(1,2,-3,4,-5,-7,-9)
 val words = listOf<String>("first","element","of","found","the","list","last")
 fun main() {
+    println("${nos.count()}digits are there")
+    println("${nos.count { it % 2 == 0 }} even nos are there")
+    
     val first = words.find { it.startsWith("f") }
     println(first)
     val last = words.findLast { it.startsWith("f") }
